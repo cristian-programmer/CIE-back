78372cceea5a01fa302dfb04ccd00609f920f744
Cristian Vargas <cvg97@misena.edu.co>
2020-05-11 01:47:20 -0500

    se trabajo en las routas de configuracion

---
+++ models/configModel.js
@@ -0,0 +1,41 @@
+const Database = require('../infrastructure/ManagerConnection').Connection;
+
+class Configuration {
+
+    constructor(data){
+        this.database = new Database();
+        this.database.getConection();
+        
+        if(data != undefined) {
+            this.nameModule =  data.nameModule;
+            this.role = data.role;
+            this.route = data.route;
+            this.active = data.active;
+        }
+    }
+
+    async getSystemModules(){
+        return await this.database.queryCommand("SELECT * FROM mydb.SystemModules")
+    }
+
+    async createModules(){
+        const result = await this.database.queryCommand(`INSERT INTO mydb.SystemModules (nameModule, role, route, active)
+         values ("${this.nameModule}", "${this.role}", "${this.route}", ${this.active})`);
+        console.log(result);
+         return result['affectedRows'] == 1 ? 'created' :  'not-created';
+    }
+
+    async updateVisible(data){
+        console.log(data);
+        const result = await this.database.queryCommand(`UPDATE mydb.SystemModules SET active=${data.visible}
+         WHERE idSystemModules=${data.id} `);
+        console.log(result);
+        return result['changedRows'] == 1 ? 'edited' :  'not-edited';
+    }
+
+}
+
+
+module.exports = {
+    Configuration: Configuration
+}
