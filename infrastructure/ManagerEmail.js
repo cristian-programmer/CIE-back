f8fea0f0a00d8d117caa8d491ab6f2379e7c70c6
Cristian Vargas <vargascristian@americana.edu.co>
2020-07-17 02:23:25 -0500

    se trabajo en el servidor de correo y en el servidor de sockets

--- infrastructure/ManagerEmail.js
+++ infrastructure/ManagerEmail.js
@@ -1,15 +1,29 @@
+const nodemailer = require('nodemailer');
+
 class ManagerEmail {
    
     constructor(){
-
+        this.transporter = nodemailer.createTransport({
+            service: 'Gmail',
+            port: 456,
+            host: 'smtp.gmail.com',
+            auth: {
+                user: 'gestioncietest@gmail.com',
+                pass: '1048323392'
+            }
+        });
     }
 
-    sendEmail(){
-
+    sendEmail(options){
+        this.transporter.sendMail(options, (error, info)=>{
+            if(error)
+                console.error(error);
+            
+            console.log("send >>>> ", info);
+        });
     }
-
-    addMesage(){
-
-    }
-
 }
+
+module.exports = {
+    ManagerEmail:ManagerEmail
+};
--- infrastructure/SocketServer.js
+++ infrastructure/SocketServer.js
@@ -1,6 +1,6 @@
 const Socket = require('socket.io');
 const UserModel = require('../models/userModel').UserModel;
-
+const ManagerEmail = require('./ManagerEmail').ManagerEmail;
 
 
 const PORT = 4000;
@@ -10,7 +10,7 @@ class SocketServer {
         this.socket = new Socket(PORT); 
         this.userModel = new UserModel();
         this.usersActives = new Map();
-        this.usersActives.set();
+        this.email = new ManagerEmail();
     }
 
     socketServerOn(){
@@ -18,6 +18,7 @@ class SocketServer {
         this.socket.on('connection', (socket)=>{
             this.registerUser(socket);
             this.inviteProject(socket);
+            this.createActivity(socket);
         });
     }
 
@@ -33,10 +34,9 @@ class SocketServer {
         socket.on('/invite', (data) => {
             console.info(` invite >>>>${data.to}`);
             const names = (data.to).toString().split(',');
-            let notifi = []; 
-
+            
             console.log(names);
-            this.usersActives.forEach((value, key) =>{
+            this.usersActives.forEach( async (value, key)  =>{
                 console.log(` value ${value} key ${key}`);
                 if(names.includes(key)){
                     console.info('include', key);
@@ -45,10 +45,48 @@ class SocketServer {
                         image: data.image,
                         message: data.message
                     });
+
+                    const result = await this.userModel.getUserByName(key);
+                    console.log(result[0].email);
+                    const options = {
+                        from: 'Remitente',
+                        to: result[0].email,
+                        subject: 'Gestion CIE - notificaciones',
+                        text: data.message
+                    };
+                    this.email.sendEmail(options);
                     this.socket.sockets.connected[value].emit("/notification", response)
                 }
             });
-            socket.emit('/invited', '<ok>');
+        });
+    }
+
+    createActivity(socket) {
+        socket.on('/createActivity', (data) => {
+           const names = (data.to).toString();
+            this.usersActives.forEach (async (value, key) =>{
+                console.log(` value ${value} key ${key}`);
+                if(names.includes(key)){
+                    console.info('include', key);
+                    const response = new Array({
+                        from: data.from,
+                        image: data.image,
+                        message: data.message
+                    });
+                    const result = await this.userModel.getUserByName(key);
+                    console.log(result[0].email);
+                    const options = {
+                        from: 'Remitente',
+                        to: result[0].email,
+                        subject: 'Gestion CIE - notificaciones',
+                        text: data.message
+                    };
+                    this.email.sendEmail(options);
+                    this.socket.sockets.connected[value].emit("/notification", response)
+
+                }
+            });
+            console.log(data);
         });
     }
 
